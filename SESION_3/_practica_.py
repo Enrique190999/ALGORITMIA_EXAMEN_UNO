df01c5c579f631f4d8b2513106812e345d30422b
Enrique <enriquepadilla1999@gmail.com>
2025-03-18 17:31:59 +0100

    Sesion 3 entradas de ejemplo para comprobar funcionamiento

--- SESION_3/_practica_.py
+++ SESION_3/_practica_.py
@@ -3,8 +3,29 @@
 def dar_la_vuelta(cambio, valores_monedas):
     pass
 
+for x in dar_la_vuelta(10,[0.50,2,0.25,5]):
+    print(x)
+    
+
 def algoritmo_mochila_voraz(objetos, peso_soportado):
     pass
 
+objetos = {
+    "Libro": (1, 4),
+    "Laptop": (3, 10),
+    "Ropa": (2, 6),
+    "Bicicleta": (4, 12)
+}
+
+peso_soportado = 7
+
+print(algoritmo_mochila_voraz(objetos,peso_soportado))
+             
+
 def algoritmo_mochila_voraz_partida(objetos, peso_soportado):
     pass
+
+peso_soportado = 7
+
+print(algoritmo_mochila_voraz_partida(objetos,peso_soportado))
+             
